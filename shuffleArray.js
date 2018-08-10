48d21ae860cb41c5be95b307a29be838950d3311
JoeyAlpha5 <35291886+JoeyAlpha5@users.noreply.github.com>
2018-08-10 04:23:36 +0200

    Create shuffleArray.js

---
+++ shuffleArray.js
@@ -0,0 +1,24 @@
+//declare an empty array for temporary use
+var tempArray = [];
+
+//call the function to shuffle your array, pass your array as a parameter and will
+//be returned as a reshuffled array e.g var array = shuffleArray(["1", "2", "3", "4"]);
+function shuffleArray(array) {
+    while (tempArray.length != array.length) {
+        for (let i = 0; i < array.length; i++) {
+            var randomNo = Math.floor(Math.random() * (array.length));
+            console.log("random number is " + randomNo);
+            var arrayText = array[randomNo];
+            if (tempArray.includes(arrayText)) {
+            }else{
+                tempArray.push(arrayText);
+            }
+        }
+
+        console.log(tempArray);
+    }
+
+    return tempArray;
+    //empty the temporary array after returning it
+    tempArray = [];
+}
